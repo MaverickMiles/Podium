433911a17a1197dac71b6dc7d6510bd565c2ec2d
William Oseghare <osegharewilliam@gmail.com>
2020-12-22 00:40:35 -0500

    Added features to thread page

--- js/modal.js
+++ js/modal.js
@@ -1,10 +1,26 @@
-var agree_button = document.getElementsByClassName("agree-button")[0];
+var agree_button = document.getElementById("user-agree-button");
+console.log(agree_button);
 var disagree_button = document.getElementsByClassName("disagree-button")[0];
 var modal = document.getElementById("myModal");
 
 
 function display_modal() {
     modal.style.display = "block";
+}
+
+function isClickable(){
+    if (document.getElementById("rule1").checked &&
+        document.getElementById("rule2").checked &&
+        document.getElementById("rule3").checked &&
+        document.getElementById("rule4").checked &&
+        document.getElementById("rule5").checked &&
+        document.getElementById("rule6").checked &&
+        document.getElementById("rule7").checked &&
+        document.getElementById("rule8").checked) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 window.addEventListener('load', (event) => {
@@ -14,7 +30,16 @@ window.addEventListener('load', (event) => {
 
 
 agree_button.onclick = function() {
-    modal.style.display = "none";
+    console.log("here");
+    if (isClickable()){
+        modal.style.display = "none";
+    }
+    else{
+        var popup = document.getElementById("myPopup2");
+        popup.classList.toggle("show");
+        setTimeout(function(){popup.classList.toggle("show");}, 5000);
+    }
+    
 }
 
 disagree_button.onclick = function() {
